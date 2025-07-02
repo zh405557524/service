7b34ea84956d4a71eb78ba393d1f621ed91372b8
zhuming <405557524@qq.com>
2025-07-02 17:53:17 +0800

    feat:初始化工程

---
+++ src/main/kotlin/com/example/service/ServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServiceApplication>(*args)
+}
